50223269f8c01ec0fd3e0365cfc4dbd18cc1b1c1
Nika <nikakotrikadze12@gmail.com>
2023-07-24 15:52:41 +0400

    Feature: added types to fetch

--- src/components/DataFetching/DataFetching.tsx
+++ src/components/DataFetching/DataFetching.tsx
@@ -2,14 +2,14 @@ import React, { useEffect } from "react";
 import axios from "axios";
 import BASE from "../../utils/API";
 import { useDataStore } from "../../store/dataStore";
-
+import { UserData } from "../../utils/UserData";
 const DataFetching = () => {
   const setData = useDataStore((state) => state.setData);
 
   useEffect(() => {
     const getData = async () => {
       try {
-        const response = await axios.get(`${BASE}/api/data`);
+        const response = await axios.get<UserData>(`${BASE}/api/data`);
         setData(response.data);
         console.log("Data received:", response.data);
       } catch (error) {
---
+++ src/utils/UserData.ts
@@ -0,0 +1,8 @@
+export interface UserData {
+  address?: object;
+  email?: string;
+  gender?: string;
+  id?: number;
+  name?: string;
+  phone?: string;
+}
