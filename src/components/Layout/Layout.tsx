dc3cf3f8b0c9c173a2df9a982a7987d8b261743c
Nika <nikakotrikadze12@gmail.com>
2023-07-24 14:26:15 +0400

    Feature: initial node setup

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DataFetching from "../DataFetching/DataFetching";
+const Layout = () => {
+  return (
+    <div>
+      <h1>hahaha Layout</h1>
+      <DataFetching />
+    </div>
+  );
+};
+
+export default Layout;
